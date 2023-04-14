da29aa294e5034675705aa811bff439eab4f8c26
William Cruz Acero <williamcruzacero@hotmail.com>
2023-04-13 23:20:27 -0500

    modificacion en server

--- sever.js
+++ sever.js
@@ -57,41 +57,6 @@ class Server {
         this.app.use(this.user, require('./routes/ruta-registro'))
         this.app.use(this.avatar, require('./routes/ruta-avatar'))
         this.app.use(this.productos, require('./routes/ruta-productos'))
-        
-        // this.app.post('/avatar', function(req, res) {           
-         
-        //     if (!req.files || Object.keys(req.files).length === 0) {
-        //       return res.status(400).send('La solicitud no contiene archivos.');
-        //     }
-          
-        //     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-        //     const {avatar} = req.files;
-
-        //     const id = crypto.randomUUID();
-
-        //     const nameSplitted = avatar.name.split('.') || [];
-        //     const extension = nameSplitted.pop() || '';
-        //     const originalName = nameSplitted.join('.');
-        //     const nameAvatar = `${originalName}_${id}.${extension}`
-
-        //     if ( !['png', 'jpg', 'tiff', 'jpeg'].includes(extension.toLocaleLowerCase()) ) {
-        //         return res.status(400).send('Extensión no soportada');
-        //     }
-
-        //     const uploadPath = __dirname + '/public/avatares/' + nameAvatar;
-          
-        //     // Use the mv() method to place the file somewhere on your server
-        //     avatar.mv(uploadPath, function(err) {
-        //       if (err)
-        //         return res.status(500).send(err);
-          
-        //       res.json({
-        //         avatar: nameAvatar
-        //       });
-        //     });
-        //   });
-      
-      
     }
 
     listen() {
