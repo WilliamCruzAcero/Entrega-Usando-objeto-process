f209674c50f89973c9bb50cbac90311962701edf
William Cruz Acero <williamcruzacero@hotmail.com>
2023-05-05 15:42:06 -0500

    tester

--- test/test_axios.js
+++ test/test_axios.js
@@ -1,22 +1,77 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// const instance = axios.create({
-//     baseURL: 'http://proyecto-coderhouse-williamcruz.onrender.com',
-//     timeout: 1000
-//     // headers: {'X-Custom-Header': 'foobar'}
-//   });
+const login = async() => {
+    try {
+        const headersList = {
+            "Accept": "*/*",
+            "Content-Type": "application/json" 
+        }
+           
+        const bodyContent = JSON.stringify({
+            "email": "williamcruzacero@hotmail.com",
+            "password": "1234"
+        });
+        
+        const reqOptions = {
+            url: "http://localhost:80/sesion",
+            method: "POST",
+            headers: headersList,
+            data: bodyContent,
+        }
+        
+        const response = await axios.request(reqOptions);
+        const {token} = response.data
+        return token
 
-// const login = async() => {
-//     try {
-//         const {token}  = await instance.post('/sesion', {
-//             email: "williamcruzacero@hotmail.com",
-//             password: "1234"
-//         });
-//         console.log(token)
+    } catch (error) {
+        throw error;
+    }
+}
 
-//     } catch (error) {
-//         console.error(error)
-//     }
-// }
+const produtos = async (token) => {
+    
+    const axiosIntance = axios.create({
+        baseURL: 'http://localhost:80',
+        timeout: 5000,
+        headers: {
+           'Content-type': 'application/json', 
+        }
+    })
 
-// login();
+    axiosIntance.interceptors.request.use(
+        config => {
+            config.headers.Authorization = token
+            return config;
+        },
+        error => {
+            console.log(error);
+            return Promise.reject(error);
+    });
+
+    const {data} = await axiosIntance.get('/api/productos')
+    return data.productos
+}
+
+const tester = async () => {
+    try {
+        const token = await login();
+        const listaProductos = await produtos(token)
+        
+        const isArray = listaProductos instanceof Array 
+        const tieneElementos = listaProductos.length > 0
+        const [produto] = listaProductos
+        const {nombre, precio, imagen, cantidad} = produto
+    
+        if ( isArray && tieneElementos && nombre && imagen && cantidad >= 0 && precio >= 0) {
+            console.log('Prueba superada')
+        } else {
+            console.log('Prueba falló')
+        }
+        
+    } catch (error) {
+        console.log(error.message)
+        console.log( 'Prueba falló' )
+    }
+}
+
+tester()
